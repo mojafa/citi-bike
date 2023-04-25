27319606b9511b6e817bb2580588a21b52f027a2
MoJafa <jmohsa@gmail.com>
2023-04-25 22:39:53 +0300

    modified test_pipeline

--- test_pipeline.py
+++ test_pipeline.py
@@ -8,15 +8,14 @@ from prefect_gcp.cloud_storage import GcsBucket
 from datetime import datetime
 
 
-
 @task
 def fetch_data():
+    """Fetch gold data from api into pandas DataFrame"""
     # insert code from above to fetch data and save to parquet file
     # set API key and base URL
     api_key = 'MZEBJG8H3L5VEMBY'
     base_url = 'https://www.alphavantage.co/query'
-
-    # set query parameters
+       # set query parameters
     params = {
         'function': 'FX_DAILY',
         'from_symbol': 'XAU',
@@ -25,7 +24,7 @@ def fetch_data():
     }
 
     # make API request and parse response as JSON
-    response = requests.get(base_url, params=params)
+    r = requests.get(base_url, params=params)
     data = json.loads(response.text)
 
     # convert JSON response to pandas dataframe
@@ -58,17 +57,7 @@ def fetch_data():
 
 @task
 def upload_to_gcs(filename):
-    client = storage.GCSClient()
-    bucket_name = 'finnhub-gcs'
-    client.upload(
-        bucket=bucket_name,
-        location=f'data/{datetime.today().strftime("%Y-%m-%d")}/{filename}',
-        filepath=filename,
-        mime_type='application/octet-stream'
-    )
-
-    with Flow('fetch_and_upload_data') as flow:
-        data = fetch_data()
-        filename = 'data.parquet'
-        upload_to_gcs(filename)
-        flow.run()
+    """Upload local parquet file to GCS"""
+    gcs_block = GcsBucket.load("finnhub-gcs")
+    gcs_block.upload_from_path(from_path=path, to_path=path)
+    return
