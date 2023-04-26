00a33e300c136ee5247eaf57f17461e6d4f8f517
MoJafa <jmohsa@gmail.com>
2023-04-26 14:55:58 +0300

    modified test_pipeline.py

--- test_pipeline.py
+++ test_pipeline.py
@@ -8,80 +8,130 @@ from prefect_gcp.cloud_storage import GcsBucket
 from datetime import datetime
 from pathlib import Path
 
+import requests
+import pandas as pd
 
+# Set API key and base URL
+api_key = 'MZEBJG8H3L5VEMBY'
+base_url = 'https://www.alphavantage.co/query'
 
-@task(log_prints=True)
-def fetch_data():
-    """Fetch gold data from api into pandas DataFrame"""
-    # insert code from above to fetch data and save to parquet file
-    # set API key and base URL
-    api_key = 'MZEBJG8H3L5VEMBY'
-    base_url = 'https://www.alphavantage.co/query'
-       # set query parameters
-    params = {
-        'function': 'FX_DAILY',
-        'from_symbol': 'XAU',
-        'to_symbol': 'USD',
-        'apikey': api_key
-    }
+# Set query parameters
+params = {
+    'function': 'TIME_SERIES_INTRADAY',
+    'symbol': 'TSLA',
+    'interval': '5min',
+    'outputsize': 'full',
+    'apikey': api_key
+}
 
-    # make API request and parse response as JSON
-    r = requests.get(base_url, params=params)
-    data = json.loads(response.text)
+# Make API request and parse response as JSON
+try:
+    response = requests.get(base_url, params=params)
+    response.raise_for_status()
+    data = response.json()
+    print(data)
 
-    # convert JSON response to pandas dataframe
-    df = pd.DataFrame.from_dict(data['Time Series FX (Daily)'], orient='index')
-    df.index.name = 'date'
+except requests.exceptions.RequestException as e:
+    print('Error:', e)
+    exit(1)
+
+# Convert JSON response to pandas DataFrame
+
+try:
+    df = pd.DataFrame.from_dict(data['Time Series (5min)'], orient='index')
+    df.index.name = 'timestamp'
     df.reset_index(inplace=True)
-    df.rename(columns={'4. close': 'close'}, inplace=True)
-    df['symbol'] = 'XAUUSD'
+    df.rename(columns={
+        '1. open': 'open',
+        '2. high': 'high',
+        '3. low': 'low',
+        '4. close': 'close',
+        '5. volume': 'volume'
+    }, inplace=True)
+    df['symbol'] = 'TSLA'
+    print(df)
 
-    # save dataframes to parquet
-    df.to_parquet('data/gold.parquet')
-    return df
+except KeyError as e:
+    print('Error:', e)
+    exit(1)
 
-@task(log_prints=True)
-def clean_data(df: pd.DataFrame) -> pd.DataFrame:
-    """Cleaning gold dataframe"""
+# Filter DataFrame to March 2023
+df['timestamp'] = pd.to_datetime(df['timestamp'])
+df = df[df['timestamp'].dt.year == 2023]
+df = df[df['timestamp'].dt.month == 3]
 
-    # convert date column to datetime
-    df['date'] = pd.to_datetime(df['date'])
+# Save DataFrame as CSV file
+df.to_csv('TSLA_March_2023.csv', index=False)
 
-    # convert close column to float
-    df['close'] = df['close'].astype(float)
-
-    print(df.head(2))
-    print(f"columns: {df.dtypes}")
-    print(f"rows: {len(df)}")
-    return df
-
-@task
-def write_local(df: pd.DataFrame, dataset_file: str) -> Path:
-    """Write cleaned data to local parquet file"""
-    Path(f"data").mkdir(parents=True, exist_ok=True)
-    df.to_parquet('data/gold.parquet')
-    return df
+# Print first few rows of the DataFrame
+print(df.head())
 
 
-@task(log_prints=True)
-def upload_to_gcs(filename):
-    """Upload local parquet file to GCS"""
-    gcs_block = GcsBucket.load("finnhub-gcs")
-    gcs_block.upload_from_path(from_path='data/gold_us30.parquet', to_path='data/gold_us30.parquet')
-    return
+# # @task(log_prints=True)
+# def fetch_data():
+#     """Fetch gold data from api into pandas DataFrame"""
+#     # insert code from above to fetch data and save to parquet file
+#     # set API key and base URL
+#     api_key = 'MZEBJG8H3L5VEMBY'
+#     base_url = 'https://www.alphavantage.co/query'
+#        # set query parameters
+#     params = {
+#         'function': 'FX_DAILY',
+#         'from_symbol': 'XAU',
+#         'to_symbol': 'USD',
+#         'apikey': api_key
+#     }
+
+#     # make API request and parse response as JSON
+#     r = requests.get(base_url, params=params)
+#     data = json.loads(response.text)
+
+#     # convert JSON response to pandas dataframe
+#     df = pd.DataFrame.from_dict(data['Time Series FX (Daily)'], orient='index')
+#     df.index.name = 'date'
+#     df.reset_index(inplace=True)
+#     df.rename(columns={'4. close': 'close'}, inplace=True)
+#     df['symbol'] = 'XAUUSD'
+
+#     # save dataframes to parquet
+#     df.to_parquet('data/gold.parquet')
+#     print(df)
+#     return df
+
+# # @task(log_prints=True)
+# # def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+# #     """Cleaning gold dataframe"""
+# #     # convert date column to datetime
+# #     df['date'] = pd.to_datetime(df['date'])
+
+# #     # convert close column to float
+# #     df['close'] = df['close'].astype(float)
+
+# #     print(df.head(2))
+# #     print(f"columns: {df.dtypes}")
+# #     print(f"rows: {len(df)}")
+# #     print(df)
+
+# # @task
+# def write_local(df: pd.DataFrame, dataset_file: str) -> Path:
+#     """Write cleaned data to local parquet file"""
+#     Path(f"data").mkdir(parents=True, exist_ok=True)
+#     df.to_parquet('data/gold.parquet')
+#     return df
 
 
-@flow()
-def web_to_gcs() -> None:
-    year = 2019
-    for month in range(1, 13):
-        dataset_file = f"fhv_tripdata_{year}-{month:02}"
-        dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/fhv/{dataset_file}.csv.gz"
-        df = fetch(dataset_url)
-        df_clean = clean(df)
-        path = write_local(df_clean, dataset_file)
-        write_gcs(path)
+# @task(log_prints=True)
+# def upload_to_gcs(filename):
+#     """Upload local parquet file to GCS"""
+#     gcs_block = GcsBucket.load("finnhub-gcs")
+#     gcs_block.upload_from_path(from_path='data/gold_us30.parquet', to_path='data/gold_us30.parquet')
+#     return
 
 
-if __name__ == "__main__":
-    web_to_gcs()
+# @flow()
+# def main() -> None:
+#     """Main flow"""
+#     df = fetch_data()
+#     df_clean = clean_data(df)
+#     path = write_local(df_clean)
+#     upload_to_gcs(path)
