bc9d128a902c32931c91f2371edc92bf621ec907
MoJafa <jmohsa@gmail.com>
2023-05-02 23:34:12 +0300

    web to gcs to bq

---
+++ prefect/web_to_gcs_to_bq.py
@@ -0,0 +1,112 @@
+import pandas as pd
+import requests
+import io
+import zipfile
+from google.cloud import storage
+import datetime as dt
+from pathlib import Path
+from prefect import flow, task
+from prefect_gcp import GcpCredentials
+from prefect_gcp.cloud_storage import GcsBucket
+# from prefect_dbt.cli import DbtCoreOperation, DbtCliProfile
+
+
+@task(log_prints=True, name="Fetch divvy data", retries=3)
+def download_file(url):
+    print(f"Downloading data from {url}...")
+    response = requests.get(url)
+    return io.BytesIO(response.content)
+
+
+@task(log_prints=True, name="Reading data as dataframe")
+def read_csv(data):
+    with zipfile.ZipFile(data, "r") as zip_ref:
+        file_list = zip_ref.namelist()
+        csv_files = [f for f in file_list if f.endswith('.csv')]
+        if len(csv_files) == 0:
+            raise ValueError("ZIP file does not contain a CSV file. Expected one CSV file.")
+
+        df_list = []
+        for csv_file in csv_files:
+            with zip_ref.open(csv_file) as f:
+                df = pd.read_csv(f, encoding='latin-1')
+                df_list.append(df)
+        df = max(df_list, key=len)
+        return df
+
+@task(log_prints=True, name="Writing to GCS bucket")
+def write_gcs(df, filename, bucket_name):
+    """Upload a pandas DataFrame as a parquet file to GCS"""
+    gcs_bucket = GcsBucket.load("citibike")
+    gcs_bucket.upload_from_dataframe(df=df, to_path=filename, serialization_format='parquet_snappy',timeout=1000)
+
+@task(log_prints=True, name="Extracting from GCS bucket")
+def extract_from_gcs() -> pd.DataFrame:
+    """Download and concatenate trip data from GCS"""
+    gcs_path = "citi_bike_datalake_citi-bike-385512/"
+    gcs_block = GcsBucket.load("citibike")
+    blobs=gcs_block.list_blobs("citi_bike_datalake_citi-bike-385512/")
+    df_list = []
+    for blob in blobs:
+        if blob.name.endswith(".parquet.snappy"):
+            df = pd.read_parquet(f"gcs://{blob.bucket.name}/{blob.name}")
+            df_list.append(df)
+    return pd.concat(df_list)
+
+
+@task(log_prints=True, name="Transformaing Data")
+def transform(df: pd.DataFrame) -> pd.DataFrame:
+    """Data cleaning and transformation"""
+    df = df.astype({
+        "ride_id": "str",
+        "rideable_type": "str",
+        "started_at": "datetime64[ns]",
+        "ended_at": "datetime64[ns]",
+        "start_station_name": "str",
+        "start_station_id": "str",
+        "end_station_name": "str",
+        "end_station_id": "str",
+        "start_lat": "float64",
+        "start_lng": "float64",
+        "end_lat": "float64",
+        "end_lng": "float64",
+        "member_casual": "str"
+    })
+    return df
+
+@task(log_prints=True, name="Writing to BQ table")
+def write_bq(df: pd.DataFrame) -> None:
+    """Write DataFrame to BiqQuery"""
+
+    gcp_credentials_block = GcpCredentials.load("citibike")
+
+    df.to_gbq(
+        destination_table="citi-bike-385512.citi_bike_rawdata",
+        project_id="citi-bike-385512",
+        credentials=gcp_credentials_block.get_credentials_from_service_account(),
+        chunksize=500_000,
+        if_exists="append",
+    )
+
+@flow
+def web_to_gcs_to_bq():
+    bucket_name = "citi_bike_datalake_citi-bike-385512"
+
+    urls = [
+        f"https://s3.amazonaws.com/tripdata/{yearmonth}-citibike-tripdata.csv.zip"
+        for year in range(2019, 2023)
+        for yearmonth in [f"{year}{month:02d}" for month in range(4, 13)] + [f"{year+1}{month:02d}" for month in range(1, 3)]
+    ]
+
+    for url in urls:
+        data = download_file(url)
+        df = read_csv(data)
+        filename = f"citibike-tripdata/{url.split('/')[-1].replace('.csv.zip', '.parquet')}"
+        write_gcs(df, filename, bucket_name)
+
+    df = extract_from_gcs()
+    df = transform(df)
+    write_bq(df)
+
+if __name__ == "__main__":
+    web_to_gcs_to_bq()
