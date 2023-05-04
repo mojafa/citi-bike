a94b025ddb177c3d1a19fe812f99b9a8aca92cd9
MoJafa <jmohsa@gmail.com>
2023-05-04 21:17:35 +0300

    woooot pipline worksgit add .

--- prefect/web_to_gcs_to_bq.py
+++ prefect/web_to_gcs_to_bq.py
@@ -18,62 +18,80 @@ def download_file(url):
 
 @task(log_prints=True, name="Reading data as dataframe")
 def read_csv(data):
+    # Read the CSV file(s) in the ZIP file
     with zipfile.ZipFile(data, "r") as zip_ref:
         file_list = zip_ref.namelist()
         csv_files = [f for f in file_list if f.endswith('.csv')]
         if len(csv_files) == 0:
             raise ValueError("ZIP file does not contain a CSV file. Expected one CSV file.")
-
-        df_list = []
         for csv_file in csv_files:
             with zip_ref.open(csv_file) as f:
-                df = pd.read_csv(f, encoding='latin-1')
-                df['start_station_id'] = pd.to_numeric(df['start_station_id'], errors='coerce').fillna(0)
-                df['end_station_id'] = pd.to_numeric(df['end_station_id'], errors='coerce').fillna(0)
-                df_list.append(df)
-        df = max(df_list, key=len)
+                df = pd.read_csv(f, encoding='latin-1', low_memory=False, dtype={
+                    "ride_id": "str",
+                    "rideable_type": "str",
+                    "started_at": "str",
+                    "ended_at": "str",
+                    "start_station_name": "str",
+                    "start_station_id": "str",
+                    "end_station_name": "str",
+                    "end_station_id": "str",
+                    "start_lat": "float64",
+                    "start_lng": "float64",
+                    "end_lat": "float64",
+                    "end_lng": "float64",
+                    "member_casual": "str"
 
-        return df
+
+                })
+                print(df.head(2))
+                print(f"columns: {df.dtypes}")
+                print(f"rows: {len(df)}")
+                # df['start_station_id'] = pd.to_numeric(df['start_station_id'], errors='coerce').fillna(0)
+                # df['end_station_id'] = pd.to_numeric(df['end_station_id'], errors='coerce').fillna(0)
+                return df
 
 @task(log_prints=True, name="Writing to GCS bucket")
 def write_gcs(df, filename, bucket_name):
     """Upload a pandas DataFrame as a parquet file to GCS"""
     gcs_bucket = GcsBucket.load("citibike")
-    gcs_bucket.upload_from_dataframe(df=df, to_path=filename, serialization_format='parquet',timeout=1000)
+    gcs_bucket.upload_from_dataframe(df=df, to_path=filename, serialization_format='csv',timeout=1000)
 
-@task(log_prints=True, name="Extracting from GCS bucket")
-def extract_from_gcs() -> pd.DataFrame:
-    """Download and concatenate trip data from GCS"""
-    gcs_path = "citi_bike_datalake_citi-bike-385512/"
-    gcs_block = GcsBucket.load("citibike")
-    blobs=gcs_block.list_blobs("citi_bike_datalake_citi-bike-385512/")
-    df_list = []
-    for blob in blobs:
-        print(blob.name)
-        if blob.name.endswith(".parquet"):
-            df = pd.read_parquet(f"gs://{blob.bucket.name}/{blob.name}")
-            df_list.append(df)
-    return pd.concat(df_list)
+# @task(log_prints=True, name="Extracting from GCS bucket")
+# def extract_from_gcs() -> pd.DataFrame:
+#     """Download and concatenate trip data from GCS"""
+#     gcs_path = "citi_bike_datalake_citi-bike-385512/"
+#     gcs_block = GcsBucket.load("citibike")
+#     blobs=gcs_block.list_blobs("citi_bike_datalake_citi-bike-385512/")
+#     df_list = []
+#     for blob in blobs:
+#         print(blob.name)
+#         if blob.name.endswith(".csv"):
+#             df = pd.read_csv(f"gs://{blob.bucket.name}/{blob.name}")
+#             # df_list.append(df)
+#     # return pd.concat(df_list)
+#     return df
 
-@task(log_prints=True, name="Transforming Data")
-def transform(df: pd.DataFrame) -> pd.DataFrame:
-    """Data cleaning and transformation"""
-    df = df.astype({
-        "ride_id": "str",
-        "rideable_type": "str",
-        "started_at": "datetime64[ns]",
-        "ended_at": "datetime64[ns]",
-        "start_station_name": "str",
-        "start_station_id": "str",
-        "end_station_name": "str",
-        "end_station_id": "str",
-        "start_lat": "float64",
-        "start_lng": "float64",
-        "end_lat": "float64",
-        "end_lng": "float64",
-        "member_casual": "str"
-    })
-    return df
+# @task(log_prints=True, name="Transforming Data")
+# def transform(df: pd.DataFrame) -> pd.DataFrame:
+#     """Data cleaning and transformation"""
+#      # df['start_station_id'] = pd.to_numeric(df['start_station_id'], errors='coerce').fillna(0)
+#     #df['end_station_id'] = pd.to_numeric(df['end_station_id'], errors='coerce').fillna(0)
+#     df = df.astype({
+#         "ride_id": "str",
+#         "rideable_type": "str",
+#         "started_at": "datetime64[ns]",
+#         "ended_at": "datetime64[ns]",
+#         "start_station_name": "str",
+#         "start_station_id": "str",
+#         "end_station_name": "str",
+#         "end_station_id": "str",
+#         "start_lat": "float64",
+#         "start_lng": "float64",
+#         "end_lat": "float64",
+#         "end_lng": "float64",
+#         "member_casual": "str"
+#     })
+#     return df
 
 @task(log_prints=True, name="Writing to BQ table")
 def write_bq(df: pd.DataFrame) -> None:
@@ -82,31 +100,38 @@ def write_bq(df: pd.DataFrame) -> None:
     gcp_credentials_block = GcpCredentials.load("citibike")
 
     df.to_gbq(
-        destination_table="citi-bike-385512.citi_bike_rawdata",
         project_id="citi-bike-385512",
+        destination_table="citi-bike-385512.citibike_dw.rides",
         credentials=gcp_credentials_block.get_credentials_from_service_account(),
         chunksize=500_000,
-        if_exists="append",
+        if_exists="replace",
     )
 
 @flow
 def web_to_gcs_to_bq():
     bucket_name = "citi_bike_datalake_citi-bike-385512"
 
+    # urls = [
+    #     f"https://s3.amazonaws.com/tripdata/202104-citibike-tripdata.csv.zip"
+
+    #     # for year in range(2020, 2023)
+    #     # year = 2020
+    #     # month = 4
+    #     # for yearmonth in [f"{year}{month:02d}" for month in range(4, 5)] + [f"{year+1}{month:02d}" for month in range(1, 2)]
+    # ]
     urls = [
-        f"https://s3.amazonaws.com/tripdata/{yearmonth}-citibike-tripdata.csv.zip"
-        for year in range(2021, 2022)
-        for yearmonth in [f"{year}{month:02d}" for month in range(4, 5)] + [f"{year+1}{month:02d}" for month in range(1, 2)]
-    ]
+    "https://s3.amazonaws.com/tripdata/202201-citibike-tripdata.csv.zip",
+    # "https://s3.amazonaws.com/tripdata/201402-citibike-tripdata.zip",
+     ]
 
     for url in urls:
         data = download_file(url)
         df = read_csv(data)
-        filename = f"{url.split('/')[-1].replace('.zip', '.parquet')}"
+        filename = f"{url[34:-4]}"
         write_gcs(df, filename, bucket_name)
 
-    df = extract_from_gcs()
-    df = transform(df)
+    # df = extract_from_gcs()
+    # df = transform(df)
     write_bq(df)
 
 if __name__ == "__main__":
